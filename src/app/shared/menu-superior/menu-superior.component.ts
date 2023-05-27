0653902ff9953f57b13d978a6ad46557b6b263b3
Guilherme Silva <developer@myvillage.com.br>
2023-05-27 15:32:45 -0300

    bet365

--- src/app/shared/menu-superior/menu-superior.component.ts
+++ src/app/shared/menu-superior/menu-superior.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-superior',
   templateUrl: './menu-superior.component.html',
-  styleUrls: ['./menu-superior.component.css']
+  styleUrls: ['./menu-superior.component.scss']
 })
 export class MenuSuperiorComponent {
   isCollapsed: boolean = true;
