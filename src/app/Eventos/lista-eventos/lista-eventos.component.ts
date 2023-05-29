19a0d206e00153583f61f181e82f08961fb6905a
Guilherme Silva <developer@myvillage.com.br>
2023-05-29 12:03:23 -0300

    ajuste

--- src/app/Eventos/lista-eventos/lista-eventos.component.ts
+++ src/app/Eventos/lista-eventos/lista-eventos.component.ts
@@ -9,7 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-lista-eventos',
   templateUrl: './lista-eventos.component.html',
-  styleUrls: ['./lista-eventos.component.css']
+  styleUrls: ['./lista-eventos.component.scss']
 })
 
 
@@ -17,6 +17,7 @@ export class ListaEventosComponent implements OnInit {
 
   public teamsNg: Team[];
   errorMessage: string;
+  isLoading: boolean = true;
   
 
   constructor(seoService: SeoService, private eventoService: EventoService,private route: ActivatedRoute){
@@ -30,19 +31,25 @@ export class ListaEventosComponent implements OnInit {
   seoService.setSeoDate(seoModel);
   }
 
-  async ngOnInit() {
-    this.route.queryParams.subscribe(params => {
-      const idLiga = params['idLiga'];
-      if (idLiga) {
-        this.eventoService.ObterTeams(idLiga).subscribe(
-          teams => {
-            this.teamsNg = teams;
-            console.log(this.teamsNg); // Adicione o console.log aqui
-          },
-          error => this.errorMessage = error
-        );
-      }
-    });
-  }
-  
+
+async ngOnInit() {
+  this.route.queryParams.subscribe(params => {
+    const idLiga = params['idLiga'];
+    if (idLiga) {
+      this.isLoading = true; // Define isLoading como true para mostrar a mensagem de carregamento
+
+      this.eventoService.ObterTeams(idLiga).subscribe(
+        teams => {
+          this.teamsNg = teams;
+          this.isLoading = false; // Define isLoading como false para indicar que a página terminou de carregar
+          console.log(this.teamsNg); // Adicione o console.log aqui
+        },
+        error => {
+          this.errorMessage = error;
+          this.isLoading = false; // Define isLoading como false mesmo em caso de erro
+        }
+      );
+    }
+  });
+}
 }
